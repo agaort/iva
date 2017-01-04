3c7d880fd7dbf593462bf2f91f8f8f4660259096
agaor <sgaort@gmail.com>
2017-01-04 15:07:57 +0100

    New module Files

--- src/config.js
+++ src/config.js
@@ -55,6 +55,11 @@ var application = {
             visibility: "public"
         },
         {
+            id: "upload",
+            title: "Upload File",
+            visibility: "public"
+        },
+        {
             id: "browser",
             title: "Variant Browser",
             visibility: "public",
